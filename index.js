cdf129141035d4b4a80b07169e1a1d23b91edf5c
Hussam Djadi <djadih@utexas.edu>
2022-11-05 17:46:33 -0500

    Object notation

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 // Place script below the elements for better UX.
 // console.log('Hello World');
 
-let name = 'Hussam';
-console.log(name);
+// let name = 'Hussam';
+// console.log(typeof(name));
 
-// learning constants
-const rate = 0.3;
-rate = 1;
-console.log(rate);
+// // learning constants
+// const rate = 0.3;
+// // console.log(rate);
+
+// let approved = true;
+// let variable = null;
+// console.log(typeof(variable));
+
+// // Dynamic typing
+// name = 4;
+// console.log(typeof(name));
+
+// Reference Types (objects)
+// let name = 'Hussam';
+// let age = 30;
+let person = {
+    name: 'Hussam',
+    age: 30
+}
+
+// Dot notation
+person.name = 'John';
+
+// Bracket notation
+let selection = 'name'
+person[selection] = 'Phil';
+
+console.log(person['name']);
