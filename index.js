c9a7eafc44dc257ebaf6b7ca81d5b808acd1a667
Hussam Djadi <djadih@utexas.edu>
2022-11-05 17:39:13 -0500

    Constants

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Place script below the elements for better UX.
-console.log('Hello World');
+// console.log('Hello World');
+
+let name = 'Hussam';
+console.log(name);
+
+// learning constants
+const rate = 0.3;
+rate = 1;
+console.log(rate);
