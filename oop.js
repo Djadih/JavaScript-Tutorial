3ff3172bd270173546a7280ed0a1d47a47453a20
Hussam Djadi <djadih@utexas.edu>
2022-11-05 18:25:55 -0500

    Constructor function

--- oop.js
+++ oop.js
@@ -24,5 +24,14 @@ function createCircle(radius) {
 }
 
 const circle = createCircle(1)
-
 console.log(circle);
+
+// Constructor function
+function Circle(radius) {
+    // console.log('this', this);
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+const another = new Circle(1);
